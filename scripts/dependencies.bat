41da06b86bb318f06b8d75bc8e6e47159c4ee819
Rameev Timur <stt1xx@mail.ru>
2025-04-19 15:04:46 +0300

    Update README.md

---
+++ scripts/dependencies.bat
@@ -0,0 +1,13 @@
+cd ../shared_module
+call gradlew build
+call gradlew publishToMavenLocal
+
+cd ../consumer
+call gradlew build
+
+cd ../frontend
+call npm run build
+cd ../backend
+call gradlew build
+
+docker-compose up --build -d
